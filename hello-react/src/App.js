1d05f06cbaf8f5686a5489a676fad26886727292
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2021-12-30 00:48:09 +0900

    1229 ref, 컴포넌트 반복, 라이프사이클 메서드
    146p-204p

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,31 @@
-import EventPractice from './EventPractice'
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
-const App = () => {
-  return <EventPractice />;
-};
-
+// 랜덤 색상을 생성합니다.
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: '#000000'
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 export default App;
 
 
--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,41 +1,51 @@
 import React, { useState } from 'react';
 
 const EventPractice = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
-    const onClick = () => {
-        alert(username + ': ' + message);
-        setUsername('');
-        setMessage('');
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const { username, message } = form;
+  const onChange = e => {
+    setTimeout(() => console.log(e), 500);
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사 한 뒤
+      [e.target.name]: e.target.value // 원하는 값을 덮어씌우기
     };
-    const onKeyPress = e => {
-        if(e.key === 'Enter'){
-            onClick();
-        }
-    };
-    return (
-        <div>
-            <h1>이벤트 연습</h1>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="사용자명"
-                    value={username}
-                    onChange={onChangeUsername}
-                />
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="아무거나 입력해 보세요"
-                    value={message}
-                    onChange={onChangeMessage}
-                    onKeyPress={onKeyPress}
-                />
-                <button onClick={onClick}>확인</button>
-        </div>
-    );
+    setForm(nextForm);
+  };
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setForm({
+      username: '',
+      message: ''
+    });
+  };
+  const onKeyPress = e => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="유저명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해보세요"
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
 };
-
 export default EventPractice;
